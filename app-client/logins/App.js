0b1a4f0a94379b03df2b8c32363af976fec98246
IndigoWizard <microclubist@gmail.com>
2020-03-30 20:41:01 +0100

    deleted hany's uncomplete work
    seems his job ain't finished and looks empty and messy

--- app-client/logins/App.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import Sign from './screen/Sign'
-export default function App() {
-  return (
-    <Sign />
-  );
-}
--- app-client/logins/utils/const.js
+++
@@ -1,4 +0,0 @@
-export const PRIMARY_BACKGROUND='#ffe';
-export const SECENDARY_BACKGROUND='#ededed';
-export const PRIMARY='#40E0D0'
-export const PRIMARY_TEXT='#40E0D080'
